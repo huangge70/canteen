2dabf994a7ddea6af91d6685ba946de88c9c5140
huangge70 <17801093123@163.com>
2019-03-01 23:02:39 +0800

    完善

--- src/main/java/com/canteen/controller/UserController.java
+++ src/main/java/com/canteen/controller/UserController.java
@@ -4,6 +4,14 @@ import com.canteen.pojo.Dish;
 import com.canteen.pojo.User;
 import com.canteen.service.UserService;
 import com.github.pagehelper.PageInfo;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +24,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -118,19 +127,81 @@ public class UserController {
         }
     }
 
+    //短信服务请求的url地址
+    public static String requestUrl="http://api.feige.ee/SmsService/Send";
+
     @RequestMapping("/enableuser")
     public String enableuser(Integer id,Model model){
         User user=new User();
         user.setId(id);
         user.setStatus(1);
         int result=userService.updateUser(user);
+        //查询出来id对应的用户
+        User currentUser=userService.selectById(id);
         if(result==1){//修改成功
+
+            //调用第三方提供的短信服务
+            try {
+                List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+                formparams.add(new BasicNameValuePair("Account","17801093123"));
+                formparams.add(new BasicNameValuePair("Pwd","96ffa3720515a770dca8b2c97"));
+                formparams.add(new BasicNameValuePair("Content","您提交的个人信息已经被管理员审核通过，您已经被授予登录系统的权限。"));
+                formparams.add(new BasicNameValuePair("Mobile",currentUser.getPhone()));
+                formparams.add(new BasicNameValuePair("SignId","97250"));
+                Post(formparams);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
             model.addAttribute("message","修改成功！");
             return "forward:/user/selectAll";
         }else{//修改失败
             model.addAttribute("message","修改失败！");
             return "admin/usermanagement";
         }
+    }
+
+    //向提供短信服务的url发送post请求
+    public static void Post( List<NameValuePair> formparams) throws Exception {
+        CloseableHttpAsyncClient httpClient = HttpAsyncClients.createDefault();
+
+        httpClient.start();
+
+        HttpPost requestPost=new HttpPost(requestUrl);
+
+        requestPost.setEntity(new UrlEncodedFormEntity(formparams,"utf-8"));
+
+        httpClient.execute(requestPost, new FutureCallback<HttpResponse>() {
+
+            public void failed(Exception arg0) {
+
+                System.out.println("Exception: " + arg0.getMessage());
+            }
+
+            public void completed(HttpResponse arg0) {
+                System.out.println("Response: " + arg0.getStatusLine());
+                try {
+
+                    InputStream stram= arg0.getEntity().getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stram));
+                    System.out.println(	reader.readLine());
+
+                } catch (UnsupportedOperationException e) {
+
+                    e.printStackTrace();
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
+
+            }
+
+            public void cancelled() {
+                // TODO Auto-generated method stub
+            }
+        }).get();
+        System.out.println("Done");
     }
 
     @RequestMapping("/selectById")
